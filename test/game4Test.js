5beccadd095af6fd4a936729182585f19820c37a
Jiham Lee <jiham7@gmail.com>
2022-02-16 23:21:54 -0600

    solved 4

--- test/game4Test.js
+++ test/game4Test.js
@@ -7,8 +7,10 @@ describe("Game4", function() {
     await game.deployed();
 
     // nested mappings are rough :}
-
-    await game.win();
+    const signer = ethers.provider.getSigner(0);
+    const address = await signer.getAddress();  
+    await game.connect(signer).write(address);
+    await game.win(address);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
