42be45a2eb024334c3717900dd5c38e6f275c0bc
Jiham Lee <jiham7@gmail.com>
2022-02-16 23:41:48 -0600

    solved 5

--- test/game5Test.js
+++ test/game5Test.js
@@ -5,10 +5,26 @@ describe("Game5", function() {
     const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
     await game.deployed();
-
+    
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+    var notFound = true;
+    var addressFound = null;
+    while(notFound) {
+      let signer = ethers.Wallet.createRandom().connect(ethers.provider);
+      const address = await signer.getAddress();
+      console.log("address: " + address);
+      if(address.substring(0,4) == "0x00") {
+        console.log("here");
+        await ethers.provider.send("hardhat_setBalance", [address, "0xffffffffffffffffffff"]);
+        await game.connect(signer).win();
+        notFound = false;
+        addressFound = address;
+      }
+    }
+    
     // good luck
 
-    await game.win();
+    // await game.win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
